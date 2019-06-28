236d3b74f04703c9d498b3c33f0f72f75c80d584
Swapnil Shalini <swapnilshalini12@gmail.com>
2019-06-29 02:03:37 +0530

    RANGMANCH WEBSITE

---
+++ rang/src/pages/Footer.jsx
@@ -0,0 +1,16 @@
+import React, {Component } from 'react';
+import {Link} from 'react-router-dom';
+import './Footer.css';
+class Footer extends Component {
+render(){
+  return(
+<footer class="footer">
+      <div class="container">
+        <span class="text-muted">RANGMANCH -ENACTUS 2019 DELHI TECHNOLOGICAL UNIVERSITY</span>
+      </div>
+    </footer>
+  );
+}
+}
+
+export default Footer;
