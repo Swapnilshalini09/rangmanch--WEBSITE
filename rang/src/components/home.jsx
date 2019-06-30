be74cd3ff51eb8f76a4b5a929bdfe15790b4bf56
Swapnil Shalini <swapnilshalini12@gmail.com>
2019-06-30 23:52:25 +0530

    commit

--- rang/src/components/home.jsx
+++ rang/src/components/home.jsx
@@ -18,14 +18,18 @@ class home extends Component {
 
 
 </p>
- <img src={require('../images/pika.jpg')} alt="Smiley face" width="400" height="300" hspace="20" align="left"/>
+
+<img src={require('../images/rag3.jpg')}  alt="Smiley face" width="400" height="300" hspace="20" align="left" fluid />
 
            Sed ut perspicia unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et qualasss molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est !
 
 
- <img src={require('../images/pika.jpg')} alt="Smiley face" width="100" height="100" hspace="10" align="left"/>
-  <img src={require('../images/pika.jpg')} alt="Smiley face" width="100" height="100" hspace="10" align="middle"/>
-   <img src={require('../images/pika.jpg')} alt="Smiley face" width="100" height="100" hspace="10" align="right"/>
+
+
+
+ <img src={require('../images/pika.jpg')} alt="Smiley face" width="100" height="100" hspace="40" align="left"/>
+  <img src={require('../images/pika.jpg')} alt="Smiley face" width="100" height="100" hspace="40" align="middle"/>
+   <img src={require('../images/pika.jpg')} alt="Smiley face" width="100" height="100" hspace="40" align="right"/>
 </p>
 
 <Footer />
